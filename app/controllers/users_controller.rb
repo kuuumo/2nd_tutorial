cc5a5d8d4adceb99392c99ecbf2e22f1df04d128
ハヤ斗スギタ <0418ya@gmail.com>
2019-06-17 08:26:24 +0900

    第11章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
     render action: :show2
-  end
-
-  def index
-    @users = User.paginate(page: params[:page])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
@@ -19,10 +20,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      remember @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_mail
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
